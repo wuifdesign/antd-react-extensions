f945f4791bcf86046e1f0d9aff2c7346b26d6927
Wuif <5678318+wuifdesign@users.noreply.github.com>
2021-09-07 13:52:01 +0200

    add extra to errorpage

--- src/lib/components/error-page/error-page.tsx
+++ src/lib/components/error-page/error-page.tsx
@@ -6,17 +6,21 @@ type ErrorKeysType = 403 | 404 | 500
 
 export type ErrorPageProps = {
   type: ErrorKeysType
-  title?: string
-  description?: string
+  icon?: React.ReactNode
+  title?: React.ReactNode
+  description?: React.ReactNode
+  extra?: React.ReactNode
 }
 
-export const ErrorPage: React.FC<ErrorPageProps> = ({ type, title, description }) => {
+export const ErrorPage: React.FC<ErrorPageProps> = ({ type, icon, title, description, extra }) => {
   const translations = useTranslations()
   return (
     <Result
+      icon={icon}
       status={type}
       title={title || translations.ErrorPage[type]?.title}
       subTitle={description || translations.ErrorPage[type]?.description}
+      extra={extra}
     />
   )
 }
